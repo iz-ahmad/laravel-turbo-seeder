c06932cc715651296df84162627efad66fd746de
iz-ahmad <n.ahmad.web.cit22@gmail.com>
2026-01-03 19:26:58 +0600

    create main TurboSeeder class with execute method for seeding operations

--- src/TurboSeeder.php
+++ src/TurboSeeder.php
@@ -1,5 +1,24 @@
 <?php
 
+declare(strict_types=1);
+
 namespace IzAhmad\TurboSeeder;
 
-class TurboSeeder {}
+use IzAhmad\TurboSeeder\DTOs\SeederConfigurationDTO;
+use IzAhmad\TurboSeeder\DTOs\SeederResultDTO;
+use IzAhmad\TurboSeeder\Services\SeederOrchestrator;
+
+class TurboSeeder
+{
+    public function __construct(
+        private readonly SeederOrchestrator $orchestrator
+    ) {}
+
+    /**
+     * Execute seeding with the given configuration.
+     */
+    public function execute(SeederConfigurationDTO $config): SeederResultDTO
+    {
+        return $this->orchestrator->execute($config);
+    }
+}
