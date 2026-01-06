a05bc8954e26e60821477442318aa118597bff70
iz-ahmad <n.ahmad.web.cit22@gmail.com>
2026-01-06 17:28:10 +0600

    update TurboSeeder facade

--- src/Facades/TurboSeeder.php
+++ src/Facades/TurboSeeder.php
@@ -1,16 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 namespace IzAhmad\TurboSeeder\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use IzAhmad\TurboSeeder\DTOs\SeederConfigurationDTO;
+use IzAhmad\TurboSeeder\DTOs\SeederResultDTO;
 
 /**
+ * @method static SeederResultDTO execute(SeederConfigurationDTO $config)
+ *
  * @see \IzAhmad\TurboSeeder\TurboSeeder
  */
 class TurboSeeder extends Facade
 {
     protected static function getFacadeAccessor(): string
     {
-        return \IzAhmad\TurboSeeder\TurboSeeder::class;
+        return 'turbo-seeder';
     }
 }
