16958490c11e259c49706af26c282e46427cad51
iz-ahmad <n.ahmad.web.cit22@gmail.com>
2026-01-09 01:58:45 +0600

    add create method in TurboSeeder for initializing TurboSeederBuilder

--- src/Facades/TurboSeeder.php
+++ src/Facades/TurboSeeder.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace IzAhmad\TurboSeeder\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use IzAhmad\TurboSeeder\Builder\TurboSeederBuilder;
 use IzAhmad\TurboSeeder\DTOs\SeederConfigurationDTO;
 use IzAhmad\TurboSeeder\DTOs\SeederResultDTO;
 
 /**
  * @method static SeederResultDTO execute(SeederConfigurationDTO $config)
+ * @method static TurboSeederBuilder create(?string $table = null)
  *
  * @see \IzAhmad\TurboSeeder\TurboSeeder
  */
@@ -18,5 +20,19 @@ class TurboSeeder extends Facade
     protected static function getFacadeAccessor(): string
     {
         return 'turbo-seeder';
+    }
+
+    /**
+     * Create a new seeder builder instance.
+     */
+    public static function create(?string $table = null): TurboSeederBuilder
+    {
+        $builder = app(TurboSeederBuilder::class);
+
+        if ($table !== null) {
+            $builder->table($table);
+        }
+
+        return $builder;
     }
 }
